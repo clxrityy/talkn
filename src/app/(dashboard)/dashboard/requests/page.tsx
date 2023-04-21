1f8fa1f5e84a0ad385056f8150bfd25e21c37dac
clxrityy <97744702+clxrityy@users.noreply.github.com>
2023-04-21 11:16:15 -0400

    middleware

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -1,11 +1,11 @@
+import FriendRequests from '@/components/FriendRequests';
 import { fetchRedis } from '@/helpers/redis';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
-import { FC } from 'react'
 
 
-const page = async() => {
+const page = async () => {
 
     const session = await getServerSession(authOptions);
     if (!session) notFound();
@@ -13,8 +13,25 @@ const page = async() => {
     // ids of people who sent current logged in user a friend request
     const incomingSenderIds = (await fetchRedis('smembers', `user:${session.user.id}:incoming_friend_requests`)) as string[];
 
+    const incomingFriendRequests = await Promise.all(
+        incomingSenderIds.map(async (senderId) => {
+            const sender = await fetchRedis('get', `user:${senderId}`) as string
+            const senderParsed = JSON.parse(sender) as User
+            return {
+                senderId,
+                senderEmail: senderParsed.email,
+            }
+        })
+    )
 
-    return <div>page</div>
+    return <main className="pt-8">
+        <h1 className="font-bold text-5xl mb-8">
+            Add a friend
+        </h1>
+        <div className='flex flex-col gap-4'>
+            <FriendRequests incomingFriendRequests={incomingFriendRequests} sessionId={session.user.id} />
+        </div>
+    </main>
 }
 
 export default page;
---
+++ src/components/FriendRequests.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+
+import axios from 'axios'
+import { Check, UserPlus, X } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { FC, useState } from 'react'
+
+interface FriendRequestsProps {
+    incomingFriendRequests: IncomingFriendRequest[]
+    sessionId: string
+}
+
+const FriendRequests: FC<FriendRequestsProps> = ({ incomingFriendRequests, sessionId }) => {
+
+    const router = useRouter();
+
+    const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(incomingFriendRequests)
+
+
+    const acceptFriend = async (senderId: string) => {
+        await axios.post('/api/friends/accept', { id: senderId });
+
+        setFriendRequests((prev) => prev.filter((request) => request.senderId !== senderId));
+
+        router.refresh();
+    }
+
+    const denyFriend = async (senderId: string) => {
+        await axios.post('/api/friends/deny', { id: senderId });
+
+        setFriendRequests((prev) => prev.filter((request) => request.senderId !== senderId));
+
+        router.refresh();
+    }
+
+
+    return <>
+        {friendRequests.length === 0 ? (
+            <p className='text-sm text-zinc-500'>
+                Nothing to show at the moment...
+            </p>
+        ) : (
+            friendRequests.map((request) => (
+                <div
+                    key={request.senderId}
+                    className='flex gap-4 items-center'
+                >
+                    <UserPlus className='text-black' />
+                    <p className='font-medium text-lg'>
+                        {request.senderEmail}
+                    </p>
+                    <button onClick={() => acceptFriend(request.senderId)} aria-label='Accept friend' className='w-8 h-8 bg-[#72cab7] hover:bg-[#559084] grid place-items-center rounded-full transition hover:shadow-md'>
+                        <Check className='font-semibold text-white w-3/4 h-3/4' />
+                    </button>
+                    <button onClick={() => denyFriend(request.senderId)} aria-label='Deny friend' className='w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md'>
+                        <X className='font-semibold text-white w-3/4 h-3/4' />
+                    </button>
+                </div>
+            ))
+        )}
+    </>
+}
+
+export default FriendRequests;
